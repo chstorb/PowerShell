cf2e6ead0df04f56578d6fe31e30e019237a9300
chstorb <christian.storb@outlook.com>
2020-08-28 15:01:56 +0200

    file added

---
+++ Modules/MyModule.psm1
@@ -0,0 +1,22 @@
+﻿# Copyright (c) STC STORB. All rights reserved.
+# Licensed under the MIT License.
+
+
+<#
+.Synopsis
+    Visual Studio Cache Cleanup
+.Description
+    Clear the Visual Studio Cache.
+#>
+function Clear-VSCache
+{
+    $BaseDir = "$($HOME)\AppData\Local\Microsoft\VisualStudio"
+    $NameToFind = "ComponentModelCache"
+
+    $items = Get-ChildItem $BaseDir -Recurse | Where-Object { $_.PSIsContainer -and $_.Name.EndsWith($NameToFind)}
+
+    foreach($item in $items) {
+        Write-Host "$($item.FullName)\*.*"
+        Remove-Item "$($item.FullName)\*.*"
+    }
+}
